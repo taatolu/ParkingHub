28e42e32d1ec3c567b5fd4ef84ece9cf57e15c8b
Yusaku <je.suis.fatigu@gmail.com>
2025-07-12 09:05:09 +0000

    chore:#12 create config(with viper)

---
+++ api/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import(
+    "github.com/spf13/viper"
+    "fmt"
+    )
+
+type Config struct{
+    DBHost  string
+    DBUser  string
+    DBPass  string
+}
+
+func LoadConfig()(*Config, error){
+    viper.SetConfigName("config")  //<--実行元が変わったときに対応できないので、相対Pathで指定すべき
+    viper.SetConfigType("ini")
+    viper.AddConfigPath(".")    //ここまでで、config.iniというファイルを指定
+    viper.AutomaticEnv()    //環境変数を優先するように指示
+    
+    //viperを使用して設定ファイル（.env/ini）を読込む
+    err := viper.ReadInConfig()
+    if err != nil{
+        return nil, fmt.Errorf("設定ファイル読込失敗 %w", err)
+    }
+    
+    //上記viperで読込んだ設定情報をconfig構造体にマッピング
+    
+    var config Config
+    err = viper.Unmarshal(&config)
+    if err != nil{
+        return nil, fmt.Errorf("設定ファイルマッピング失敗 %w", err)
+    }
+    
+    //マッピング成功
+    return &config, nil
+}
+
+
