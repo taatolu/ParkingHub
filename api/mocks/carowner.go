b67de1c7a4b34f467e3d58fd201355e352f8c983
Yusaku <je.suis.fatigu@gmail.com>
2025-07-15 05:43:21 +0000

    Create Mock(FinedByID)

--- api/mocks/carowner.go
+++ api/mocks/carowner.go
@@ -9,15 +9,22 @@ import (
 type MockCarOwnerRepo struct{
     //リポジトリインターフェース実行の結果と照らし合わせたい内容をメンバとして定義
     SavedOwner  *model.CarOwner
-    ExistUser   *model.CarOwner
     SaveErr     error
+    FinededUser *model.CarOwner
     
 }
 
 
 //リポジトリインターフェースのメソッドシグネチャを満たすモックのメソッドを作成
 //saveメソッド
-func (m *MockCarOwnerRepo) Save(carOwner *model.CarOwner) error {
+func (m *MockCarOwnerRepo) Save (carOwner *model.CarOwner) error {
     m.SavedOwner = carOwner
     return m.SaveErr
 }
+
+//FindByID
+func (m *MockCarOwnerRepo) FindByID(id int)(*model.CarOwner, error){
+    return m.FinededUser, nil
+}
+
+
