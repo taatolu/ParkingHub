a11bf5934ee3bf153c39a1b545597d849bfd0f04
Yusaku <je.suis.fatigu@gmail.com>
2025-07-23 05:25:30 +0000

    feature: Create presentation/http/router.go

--- api/presentation/http/router.go
+++ api/presentation/http/router.go
@@ -2,10 +2,11 @@ package http
 
 import (
     "net/http"
+    "github.com/taatolu/ParkingHub/api/presentation/handler"
     )
 
 func InitRouters()http.Handler{
-    http.HandleFunc("/api/v1/car_owners", *handler.CarOwnerHandler)   //GET(ALL),POST メソッド
-    http.HandleFunc("/api/v1/car_owners/", *handler.CarOwnersHandler)    //GET,PUSH,DELETE メソッド
+    http.HandleFunc("/api/v1/car_owners", handler.CarOwnerHandler)   //GET(ALL),POST メソッド
+    http.HandleFunc("/api/v1/car_owners/", handler.CarOwnersHandler)    //GET,PUSH,DELETE メソッド
     return http.DefaultServeMux
 }
