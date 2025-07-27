13d0dbcea13da9fbe9afce82fc59f0cd9dee65f5
USER <user@USERnoMacBook-puro.local>
2025-07-27 19:20:51 +0900

    feat: Add carowner_handler_test(in progress)

---
+++ api/presentation/http/handler/carowner_handler_test.go
@@ -0,0 +1,24 @@
+package handler
+
+import(
+	"github.com/taatolu/ParkingHub/api/usecase"
+)
+
+//CarOwneeUsecaseのモック作成
+type MockCarOwnerUsecase struct{
+	//usecase/carowner_handler.goのcarOwnerのメソッドセットを満たす
+	RegistCarOwnerFunc func(owner *model.CarOwner) error
+}
+
+func (m *MockCarOwnerUsecase) RegistCarOwner(owner *model.CarOwner) error {
+	if m.RegistCarOwnerFunc != nil{
+		//RegistCarOwnerFuncがnilでなかったら
+		return m.RegistCarOwnerFunc(owner)
+	}
+	return nil
+}
+
+// httptest.NewRecorder() でレスポンス記録
+// http.NewRequest() でリクエスト作成
+// handler.ServeHTTPで実行
+// recorder.Result() でレスポンス検証
