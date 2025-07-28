e3e661077174ae0585eaf7b49051b6b22ef67f9f
Yusaku <je.suis.fatigu@gmail.com>
2025-07-28 00:04:45 +0000

    feature: Update carowner_handler_test(in progress)

--- api/presentation/http/handler/carowner_handler_test.go
+++ api/presentation/http/handler/carowner_handler_test.go
@@ -1,8 +1,10 @@
 package handler
 
 import(
-	"github.com/taatolu/ParkingHub/api/usecase"
 	"github.com/taatolu/ParkingHub/api/domain/model"
+	"testing"
+	"net/http/httptest"
+	"net/http"
 )
 
 //CarOwneeUsecaseのモック作成
@@ -19,7 +21,37 @@ func (m *MockCarOwnerUsecase) RegistCarOwner(owner *model.CarOwner) error {
 	return nil
 }
 
-// httptest.NewRecorder() でレスポンス記録
-// http.NewRequest() でリクエスト作成
-// handler.ServeHTTPで実行
-// recorder.Result() でレスポンス検証
+//テストの実行
+func TestRegistCarOwner(t *testing.T){
+    //CarOwnerUsecaseモックのインスタンス化
+    mockUsecase := &MockCarOwnerUsecase{
+        RegistCarOwnerFunc : func(owner *model.CarOwner) error{
+            return nil
+        }
+    }
+    
+    //ハンドラーのインスタンス生成
+    handler := &CarOwnerHandler{Usecase: mockUsecase}
+    
+    // httptest.NewRecorder() でレスポンス記録
+    rec := httptest.NewRecorder()
+    
+    // http.NewRequest() でリクエスト作成
+    req, err := http.NewRequest("POST", "/api/v1/car_owners", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+    
+    // handler.ServeHTTPで実行
+    handler.ServeHTTP(rec, req)
+    
+    // recorder.Result() でレスポンス検証
+    resp := rec.Result()
+    defer resp.Body.Close()
+    
+    if resp.StatusCode != http.StatusCreated {
+        t.Errorf("GotStatus=%d WantStatus=%d", resp.StatusCode, http.StatusCreated)
+    }
+
+}
+
