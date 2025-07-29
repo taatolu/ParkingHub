188032fb7d99c30b62e6cf1c6702b1234523a3e5
yusaku <je.suis.fatigu@gmail.com>
2025-07-29 07:31:41 +0000

    fix: Create FakeCarOwnerUsecase

---
+++ api/mocks/usecase/fake_carowner.go
@@ -0,0 +1,31 @@
+package usecase
+
+import(
+    "fmt"
+    "github.com/taatolu/ParkingHub/api/domain/model"
+    "github.com/taatolu/ParkingHub/api/domain/service"
+    )
+
+type FakeCarOwnerUsecase struct{
+    RgistCarOwnerFunc   func(owner *model.CarOwner) error
+}
+
+func (f *FakeCarOwnerUsecase) RegistCarOwner(owner *model.CarOwner) error {
+    //入力漏れの確認
+    if !service.CarOwnerNameValidation(owner){
+        //CarOwnerNameValidationがFalseだったら
+		return fmt.Errorf("少なくとも２つ以上のフィールドに名前を入力ください")
+    }
+    
+    //免許期限切れかどうかの確認
+    if owner.IsLicenseExpired(){
+        //IsLicenseExpiredがtrueだったら（期限切れだったら）、、、
+		return fmt.Errorf("免許証期限切れの為登録不可 %v", owner.LicenseExpiration)
+    }
+    
+    // RegistCarOwnerFuncがセットされていれば呼び出し、なければnilを返す
+    if f.RegistCarOwnerFunc != nil {
+        return f.RegistCarOwnerFunc(owner)
+    }
+    return nil
+}
