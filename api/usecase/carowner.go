f0765291327741685483b592cf7b9c4c717a50b7
Yusaku <je.suis.fatigu@gmail.com>
2025-08-05 06:42:55 +0000

    feature:FindByID(MockCarOwnerUsecase)

--- api/usecase/carowner.go
+++ api/usecase/carowner.go
@@ -29,7 +29,7 @@ func (uc *CarOwnerUsecase) RegistCarOwner(owner *model.CarOwner) error {
 	//免許証期限が切れている場合(若しくはIsLicenseExpiredが入力されていない場合)、エラーを返す
 	if owner.IsLicenseExpired() {
 		//IsLicenseExpiredがtrueだったら（期限切れだったら）、、、
-		return fmt.Errorf("免許証期限切れの為登録不可 %v", owner.LicenseExpiration)
+		return fmt.Errorf("免許証期限切れの為登録不可: %v", owner.LicenseExpiration)
 	}
 
 	return uc.CarOwnerRepo.Save(owner)
@@ -38,6 +38,10 @@ func (uc *CarOwnerUsecase) RegistCarOwner(owner *model.CarOwner) error {
 
 // owner検索（ID）
 func (uc *CarOwnerUsecase) FindByID(id int) (*model.CarOwner, error) {
+    //idのバリデーション
+    if id<0 {
+        return fmt.Errorf("IDは負の値は不可: ID=%v", id)
+    }
     return uc.FindByID(id)
 }
 
