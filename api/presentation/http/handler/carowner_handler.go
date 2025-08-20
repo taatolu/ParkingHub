ccb3c20c3f362448c083db6771044421a98c5350
Yusaku <je.suis.fatigu@gmail.com>
2025-08-20 08:32:16 +0000

    CarOwnersHandlerの仮実装

--- api/presentation/http/handler/carowner_handler.go
+++ api/presentation/http/handler/carowner_handler.go
@@ -15,6 +15,11 @@ type CarOwnerHandler struct{
     Usecase usecase.CarOwnerUsecaseIF
 }
 
+type CarOwnersHandler struct{
+    //usecase層のインターフェースから実装
+    Usecase usecase.CarOwnerUsecaseIF
+}
+
 // CarOwnerHandler definition（ルーターでCarOwnerHandlerが呼ばれたときどのメソッドを実行するか & ServeHTTPをラップ）
 func (h CarOwnerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
     switch r.Method {
@@ -24,6 +29,17 @@ func (h CarOwnerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
                 w.Write([]byte(fmt.Sprintf(`{"error":"リクエストメソッドが不正です"}`)))
     }
 }
+
+// CarOwnersHandler definition（ルーターでCarOwnersHandlerが呼ばれたときどのメソッドを実行するか & ServeHTTPをラップ）
+func (h CarOwnersHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+    switch r.Method {
+    case http.MethodGet:  h.FindByID(w, r)
+    default:    w.Header().Set("Content-Type", "application/json")
+                w.WriteHeader(http.StatusMethodNotAllowed)
+                w.Write([]byte(fmt.Sprintf(`{"error":"リクエストメソッドが不正です"}`)))
+    }
+}
+
 
 //POST api car_owners
 func (h CarOwnerHandler) CreateCarOwner(w http.ResponseWriter, r *http.Request){
