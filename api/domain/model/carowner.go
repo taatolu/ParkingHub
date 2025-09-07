2801a520e0716231d410fa1089ba0f276310750b
Yusaku <je.suis.fatigu@gmail.com>
2025-09-07 18:31:15 +0900

    fix: #76 CarOwner構造体のフィールドをgorm.Modelに統合し 、テーブル名を明示的に指定 fix: docker-compose.ymlの修正とGORM対応
    fix: #76 Dockerfileとdocker-compose.ymlの修正、GORMのインストールを追加

--- api/domain/model/carowner.go
+++ api/domain/model/carowner.go
@@ -3,25 +3,34 @@ package model
 import (
 	"strings"
 	"time"
+	"gorm.io/gorm"
 )
 
-// CarOuner構造体
+// CarOwner構造体
 type CarOwner struct {
-	ID                int //社員ID（重複無し）
-	FirstName         string
-	MiddleName        string
-	LastName          string
-	LicenseExpiration time.Time //免許証の期限
+	ID                uint		`json:"id" gorm:"primaryKey"`
+	FirstName         string	`json:"first_name" gorm:"column:first_name;type:varchar(25);not null"`
+	MiddleName        string	`json:"middle_name" gorm:"column:middle_name;type:varchar(25)"`
+	LastName          string	`json:"last_name" gorm:"column:last_name;type:varchar(25)"`
+	LicenseExpiration time.Time `json:"license_expiration" gorm:"column:license_expiration;type:date;not null"`
+	CreatedAt         time.Time `json:"created_at" gorm:"autoCreateTime"`
+    UpdatedAt         time.Time	`json:"updated_at" gorm:"autoUpdateTime"`
+    DeletedAt         gorm.DeletedAt	`json:"-" gorm:"index"`
+}
+
+//テーブル名を明示的に指定
+func (CarOwner) TableName() string {
+	return "car_owners"
 }
 
 // 免許証の期限切れ確認
 func (c *CarOwner) IsLicenseExpired() bool {
-	//現在時刻が c.LisenceExpiration より後かどうか(現在時刻が後ならtrueを返す)
+	//現在時刻が c.LicwenseExpiration より後かどうか(現在時刻が後ならtrueを返す)
 	return time.Now().After(c.LicenseExpiration)
 }
 
 //name枠に該当する名前があるか確認する
-func (c *CarOwner) IsContainsName(name string) bool {
+func (c *CarOwner) IsContainsName (name string) bool {
 	return strings.Contains(c.FirstName, name) ||
 		strings.Contains(c.MiddleName, name) ||
 		strings.Contains(c.LastName, name)
