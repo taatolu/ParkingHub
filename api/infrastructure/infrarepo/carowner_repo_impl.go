4845ed36ee77a30917f030fb18f8b83c846e4d39
Yusaku <je.suis.fatigu@gmail.com>
2025-09-09 06:06:02 +0000

    fix: sqlmockからgormを使用した記述に変更
    infrastructure/infrarepo/carowner_repo_impl.go(Saveメソッド)

--- api/infrastructure/infrarepo/carowner_repo_impl.go
+++ api/infrastructure/infrarepo/carowner_repo_impl.go
@@ -13,19 +13,8 @@ type CarOwnerRepositoryImpl struct{
 //repositoryの具体的な実装部分
 ///CarOwner_Save
 func (r *CarOwnerRepositoryImpl) Save (carOwner *model.CarOwner) error {
-	cmd := `INSERT INTO carowners (
-	ID,
-	FirstName,
-	MiddleName,
-	LastName,
-	LicenseExpiration) VALUES ($1, $2, $3, $4, $5)`
 
-	_, err := r.DB.Exec(cmd,
-		carOwner.ID,
-		carOwner.FirstName,
-		carOwner.MiddleName,
-		carOwner.LastName,
-		carOwner.LicenseExpiration)
+	err := r.DB.Create(carOwner).Error
 	if err != nil{
 		return fmt.Errorf("CarOwnerのデータ登録失敗: %w", err)
 	}
