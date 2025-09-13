4e450c5826b9f91e5c9ee73e58021921d36f2ba0
Yusaku <je.suis.fatigu@gmail.com>
2025-09-13 19:03:39 +0900

    Feat: CarOwnerRepositoryインターフェースにUpdateメソッドを追加し、モックリポジトリのテストを実装

--- api/domain/repository/carowner.go
+++ api/domain/repository/carowner.go
@@ -8,6 +8,6 @@ type CarOwnerRepository interface {
 	Save(carOwner *model.CarOwner) error
 	FindByID(id uint) (*model.CarOwner, error)
 	FindByName(name string)([]*model.CarOwner, error)    //nameに一致する社員の一覧取得
-	//Update(carOwner *model.CarOwner)error
+	Update(carOwner *model.CarOwner)error
 	//Delete(id int)error
 }
