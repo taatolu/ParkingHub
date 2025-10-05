4dd67fb1bfdf672390472e0f9340a323799553dd
Yusaku <je.suis.fatigu@gmail.com>
2025-10-05 16:56:23 +0900

    feat: OwnerDashboardコンポーネントとHeaderコンポーネントを追加し、Appコンポーネントを更新

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import OwnerDashboard from './pages/OwnerDashbord';
+import OwnerDashboard from './pages/OwnerDashboard';
+import Header from './layouts/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <OwnerDashboard />
     </div>
   )
---
+++ front/src/layouts/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../assets/css/Header.module.css";
+
+
+const Header: React.FC = () => (
+    //アロー関数でアローの右側を（）で囲む場合はreturn不要
+    <header className={styles.header}>
+        <h1 className={styles.title}>ParkingHub</h1>
+    </header>
+);
+
+export default Header;
