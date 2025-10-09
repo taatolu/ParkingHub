e144d2171f0e28dcccf3a2e29e75a729ee426d85
Yusaku <je.suis.fatigu@gmail.com>
2025-10-09 07:51:19 +0000

    Carエンティティのビジネスロジックテストを作成(任意保険期限)

--- api/domain/model/car_tset.go
+++ api/domain/model/car_tset.go
@@ -35,3 +35,34 @@ func TestIsShakenExpired(t *testing.T) {
 }
 
 //任意保険の期限切れ確認
+func TestIsInsuranceExpired(t *testing.T) {
+    //tableTest
+    tests := [] struct {
+        testname    string
+        insuranceExp    time.Time
+        wantError   bool
+    }{
+        //tsetCase
+        {
+            testname:"正常系",
+            insuranceExp:time.Now().AddDate(1, 0, 0),
+            wantError:  false,
+        },
+        {
+            testname:"異常系（任意保険切れ）",
+            insuranceExp:time.Now().AddDate(-1, 0, 0),
+            wantError:  true,
+        },
+    }
+    //テストケースをループ処理
+    for _,tt := range tests {
+        t.Run(tt.testname, func(t *testing.T){
+            car := &Car{IsShakenExpiration: tt.insuranceExp}
+            if car.IsInsuranceExpired() {
+                assert.True(t, tt.wantError)
+            } else {
+                assert.False(t, tt.wantError)
+            }
+        })
+    }
+}
